f2f58559864bb01891818f67289b11453a454224
ian-ramos <ramos.ianc@gmail.com>
2017-10-17 16:53:38 -0400

    Done.

---
+++ db/migrate/20171017172049_add_description_and_inventory_count.rb
@@ -0,0 +1,6 @@
+class AddDescriptionAndInventoryCount < ActiveRecord::Migration
+  def change
+    add_column :products, :description, :string
+    add_column :products, :inventory, :integer
+  end
+end
